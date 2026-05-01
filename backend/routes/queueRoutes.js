8756f1c85123a3c18913fb7318b33eb7f8b1e9b9
logymagdy <logymagdy8@gmail.com>
2026-05-01 15:38:32 +0300

    updated queueRoutes.js

--- backend/routes/queueRoutes.js
+++ backend/routes/queueRoutes.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { getLiveQueue } = require("../controllers/queueController");
+const { protect, allowRoles } = require("../middleware/authMiddleware");
+
+// RECEPTIONIST or serviceProvider views live queue
+router.get(
+  "/live",
+  protect,
+  allowRoles("RECEPTIONIST", "serviceProvider"),
+  getLiveQueue
+);
 
 module.exports = router;
