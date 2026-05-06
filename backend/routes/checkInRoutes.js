50114aa8ee3bb1a02ebb586409cb58f81cbea6bb
logymagdy <logymagdy8@gmail.com>
2026-05-06 22:31:34 +0300

    added checkIn tag in checkInRoutes

--- backend/routes/checkInRoutes.js
+++ backend/routes/checkInRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+// #swagger.tags = ['Check-In']
+
 const { checkIn } = require("../controllers/checkInController");
 const { protect, allowRoles } = require("../middleware/authMiddleware");
 
