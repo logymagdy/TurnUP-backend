5a291e53355170729b761f719ba12cbf4ba4e7c5
logymagdy <logymagdy8@gmail.com>
2026-05-16 22:10:50 +0300

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -47,6 +47,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ─── DATABASE ─────────────────────────────────────────────────────────────────
 connectDB();
