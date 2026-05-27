7efeab7a40eeda6e1030eb175a589bb6aae0037b
logymagdy <logymagdy8@gmail.com>
2026-05-27 23:00:11 +0300

    updated notificationRoutes

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -7,10 +7,12 @@ const {
   markAsRead,
   markAllAsRead,
   savePushToken,
+  getUnreadCount,
 } = require("../controllers/notificationController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/", protect, getMyNotifications);
+router.get("/unread-count", protect, getUnreadCount);
 router.patch("/:notificationId/read", protect, markAsRead);
 router.patch("/read-all", protect, markAllAsRead);
 router.post("/push-token", protect, savePushToken);
